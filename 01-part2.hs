461e262de9605c28ef8133c0899923988fad5a41
Elias Aebi <eyelash@users.noreply.github.com>
2019-12-07 17:04:31 +0100

    use Int instead of Integer

--- 01-part2.hs
+++ 01-part2.hs
@@ -1,8 +1,9 @@
-main = readFile "01-input.txt" >>= (putStrLn . show . processLines . lines)
+main = readFile "01-input.txt" >>= putStrLn . show . processLines . lines
 
-processLines :: [String] -> Integer
+processLines :: [String] -> Int
 processLines (line : rest) = computeFuel (read line) + processLines rest
 processLines [] = 0
 
+computeFuel :: Int -> Int
 computeFuel mass = let fuel = mass `div` 3 - 2 in
                    if fuel <= 0 then 0 else fuel + computeFuel fuel
--- 01.hs
+++ 01.hs
@@ -1,7 +1,8 @@
-main = readFile "01-input.txt" >>= (putStrLn . show . processLines . lines)
+main = readFile "01-input.txt" >>= putStrLn . show . processLines . lines
 
-processLines :: [String] -> Integer
+processLines :: [String] -> Int
 processLines (line : rest) = computeFuel (read line) + processLines rest
 processLines [] = 0
 
+computeFuel :: Int -> Int
 computeFuel mass = mass `div` 3 - 2
