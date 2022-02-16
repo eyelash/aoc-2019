593eb14091d7876c94163ada19903b2206b0b5c0
Elias Aebi <eyelash@users.noreply.github.com>
2022-02-16 10:38:33 +0100

    indent with spaces

--- 02.lean
+++ 02.lean
@@ -1,35 +1,35 @@
 def restore (p : Array Nat) (noun verb : Nat) : Array Nat :=
-	let p := p.set! 1 noun
-	let p := p.set! 2 verb
-	p
+  let p := p.set! 1 noun
+  let p := p.set! 2 verb
+  p
 
 def getOp : Nat -> Nat -> Nat -> Nat := fun
-	| 1 => (. + .)
-	| 2 => (. * .)
-	| _ => panic! "invalid opcode"
+  | 1 => (. + .)
+  | 2 => (. * .)
+  | _ => panic! "invalid opcode"
 
 partial def runProgram (p : Array Nat) (i : Nat) : Nat :=
-	if p[i] == 99 then
-		p[0]
-	else
-		let opcode := p[i]
-		let arg1 := p[p[i+1]]
-		let arg2 := p[p[i+2]]
-		let dst := p[(i+3)]
-		let p := p.set! dst (getOp opcode arg1 arg2)
-		runProgram p (i + 4)
+  if p[i] == 99 then
+    p[0]
+  else
+    let opcode := p[i]
+    let arg1 := p[p[i+1]]
+    let arg2 := p[p[i+2]]
+    let dst := p[(i+3)]
+    let p := p.set! dst (getOp opcode arg1 arg2)
+    runProgram p (i + 4)
 
 def findInputs (p : Array Nat) : IO Unit := do
-	for noun in [0:100] do
-		for verb in [0:100] do
-			let result := runProgram (restore p noun verb) 0
-			if result == 19690720 then
-				IO.println s!"noun = {noun}"
-				IO.println s!"verb = {verb}"
-				IO.println s!"100 * noun + verb = {100 * noun + verb}"
-				return
+  for noun in [0:100] do
+    for verb in [0:100] do
+      let result := runProgram (restore p noun verb) 0
+      if result == 19690720 then
+        IO.println s!"noun = {noun}"
+        IO.println s!"verb = {verb}"
+        IO.println s!"100 * noun + verb = {100 * noun + verb}"
+        return
 
 def main : IO Unit := do
-	let file <- IO.FS.readFile "02-input.txt"
-	let p := (file.trim.splitOn ",").map String.toNat!
-	findInputs p.toArray
+  let file <- IO.FS.readFile "02-input.txt"
+  let p := (file.trim.splitOn ",").map String.toNat!
+  findInputs p.toArray
--- 02.rs
+++ 02.rs
@@ -1,47 +1,47 @@
 fn main() {
-	let file = std::fs::read_to_string("02-input.txt").unwrap();
-	let p = file.trim().split(',').map(str::parse).map(Result::unwrap).collect();
-	find_inputs(p);
+  let file = std::fs::read_to_string("02-input.txt").unwrap();
+  let p = file.trim().split(',').map(str::parse).map(Result::unwrap).collect();
+  find_inputs(p);
 }
 
 fn restore(mut p: Vec<i32>, noun: i32, verb: i32) -> Vec<i32> {
-	p[1] = noun;
-	p[2] = verb;
-	p
+  p[1] = noun;
+  p[2] = verb;
+  p
 }
 
 fn get_op(opcode: i32) -> fn(i32, i32) -> i32 {
-	use std::ops::{ Add, Mul };
-	match opcode {
-		1 => i32::add,
-		2 => i32::mul,
-		_ => unreachable!(),
-	}
+  use std::ops::{ Add, Mul };
+  match opcode {
+    1 => i32::add,
+    2 => i32::mul,
+    _ => unreachable!(),
+  }
 }
 
 fn run_program(mut p: Vec<i32>) -> i32 {
-	let mut i = 0;
-	while p[i] != 99 {
-		let opcode = p[i];
-		let arg1 = p[p[i+1] as usize];
-		let arg2 = p[p[i+2] as usize];
-		let dst = p[i+3] as usize;
-		p[dst] = get_op(opcode)(arg1, arg2);
-		i += 4;
-	}
-	p[0]
+  let mut i = 0;
+  while p[i] != 99 {
+    let opcode = p[i];
+    let arg1 = p[p[i+1] as usize];
+    let arg2 = p[p[i+2] as usize];
+    let dst = p[i+3] as usize;
+    p[dst] = get_op(opcode)(arg1, arg2);
+    i += 4;
+  }
+  p[0]
 }
 
 fn find_inputs(p: Vec<i32>) {
-	for noun in 0..=99 {
-		for verb in 0..=99 {
-			let result = run_program(restore(p.clone(), noun, verb));
-			if result == 19690720 {
-				println!("noun = {}", noun);
-				println!("verb = {}", verb);
-				println!("100 * noun + verb = {}", 100 * noun + verb);
-				return;
-			}
-		}
-	}
+  for noun in 0..=99 {
+    for verb in 0..=99 {
+      let result = run_program(restore(p.clone(), noun, verb));
+      if result == 19690720 {
+        println!("noun = {}", noun);
+        println!("verb = {}", verb);
+        println!("100 * noun + verb = {}", 100 * noun + verb);
+        return;
+      }
+    }
+  }
 }
